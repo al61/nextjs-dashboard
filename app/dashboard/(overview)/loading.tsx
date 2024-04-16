a731f54152e928a923d45a1e4f8a28c1e2606483
Al Hammad <ahammad79@gmail.com>
2024-04-16 13:32:49 -0700

    Implement streaming, Suspense, and loading skeletons

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+/* Special Next.js file built on top of Suspense, it allows you to create fallback UI to show as a replacement while page content loads. */
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
