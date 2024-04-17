48ab696ef7785f68a272084c9ba67e76cbc99789
Al Hammad <ahammad79@gmail.com>
2024-04-17 16:18:20 -0700

    Implement invoices search and update tthe table

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 <img src="./public/hero-desktop.png" alt="Acme full web application" style="max-width: 100%;"/>
 
 Next.js App Router Course. See the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+### Few things done to optimize data fetching in the application:
+
+1. Created a database in the same region as the application code to reduce latency between the server and database.
+2. Fetched data on the server with React Server Components. This allows you to keep expensive data fetches and logic on the server, reduces the client-side JavaScript bundle, and prevents the database secrets from being exposed to the client.
+3. Used SQL to only fetch the data you needed, reducing the amount of data transferred for each request and the amount of JavaScript needed to transform the data in-memory.
+4. Parallelize data fetching with JavaScript - where it made sense to do so.
+5. Implemented Streaming to prevent slow data requests from blocking the whole page, and to allow the user to start interacting with the UI without waiting for everything to load.
+6. Move data fetching down to the components that need it, thus isolating which parts of the routes should be dynamic in preparation for Partial Prerendering.
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,26 @@
-'use client';
+'use client'; // This is a Client Component, which means you can use event listeners and hooks.
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname(); // current path, eg "/dashboard/invoices".
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    // As the user types into the search bar, params.toString() translates this input into a URL-friendly format
+    // Update the URL with the user's search data. For example, /dashboard/invoices?query=al if the user searches for "Al".
+    // The URL is updated without reloading the page, thanks to Next.js's client-side navigation
+    replace(`${pathname}?${params.toString()}`); /* update the URL */
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +29,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
