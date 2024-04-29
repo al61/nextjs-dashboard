42d2311c34bbb1f813aa9fea8bdaf4210361f2b7
Al Hammad <ahammad79@gmail.com>
2024-04-29 13:41:57 -0700

    Add dashboard customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <Suspense fallback={<InvoicesTableSkeleton />}>
+      <CustomersTable query={query} />
+    </Suspense>
+  );
 }
